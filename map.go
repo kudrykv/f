5fdbc8ad3fc2934b3b3a9fda53c4e36781f17fbe
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-01-18 10:07:53 +0100

    map err

--- map.go
+++ map.go
@@ -1,5 +1,7 @@
 package f
 
+import "fmt"
+
 func Map[In, Out any](list []In, mapper func(In) Out) []Out {
 	if len(list) == 0 {
 		return nil
@@ -12,4 +14,23 @@ func Map[In, Out any](list []In, mapper func(In) Out) []Out {
 	}
 
 	return result
+}
+
+func MapErr[In, Out any](list []In, mapper func(In) (Out, error)) ([]Out, error) {
+	if len(list) == 0 {
+		return nil, nil
+	}
+
+	var (
+		err    error
+		result = make([]Out, len(list))
+	)
+
+	for i := range list {
+		if result[i], err = mapper(list[i]); err != nil {
+			return nil, fmt.Errorf("map %d: %w", i, err)
+		}
+	}
+
+	return result, err
 }
--- map_test.go
+++ map_test.go
@@ -1,6 +1,7 @@
 package f_test
 
 import (
+	"errors"
 	"slices"
 	"strconv"
 	"testing"
@@ -40,6 +41,70 @@ func TestMap(t *testing.T) {
 
 		if result != nil {
 			t.Fatalf("Map(%v) = %v, want %v", list, result, []string(nil))
+		}
+	})
+}
+
+func TestMapErr(t *testing.T) {
+	t.Parallel()
+
+	t.Run("non-empty list", func(t *testing.T) {
+		t.Parallel()
+
+		list := []int{1, 2, 3}
+		expected := []string{"2", "4", "6"}
+
+		mapper := func(value int) (string, error) { return strconv.Itoa(value * 2), nil }
+
+		actual, err := f.MapErr(list, mapper)
+
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		if !slices.Equal(expected, actual) {
+			t.Fatalf("Map(%v) = %v, want %v", list, actual, expected)
+		}
+	})
+
+	t.Run("empty list", func(t *testing.T) {
+		t.Parallel()
+
+		var list []int
+
+		mapper := func(value int) (int, error) { return value * 2, nil }
+
+		result, err := f.MapErr(list, mapper)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		if len(result) > 0 {
+			t.Fatalf("Map(%v) = %v, want %v", list, result, []string(nil))
+		}
+
+		if result != nil {
+			t.Fatalf("Map(%v) = %v, want %v", list, result, []string(nil))
+		}
+	})
+
+	t.Run("mapper error", func(t *testing.T) {
+		t.Parallel()
+
+		list := []int{1, 2, 3}
+		sentinel := errors.New("sentinel")
+
+		mapper := func(value int) (string, error) {
+			if value == 2 {
+				return "", sentinel
+			}
+
+			return strconv.Itoa(value * 2), nil
+		}
+
+		_, err := f.MapErr(list, mapper)
+		if !errors.Is(err, sentinel) {
+			t.Fatalf("Expected %v, got %v", sentinel, err)
 		}
 	})
 }
